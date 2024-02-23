7ed203f7d51eb63c8d5abca7a12fc30dfcfa8359
sahilmoktan <moktanshail9@gmail.com>
2024-02-23 23:04:04 +0545

    another fauiler try

--- routes/index.js
+++ routes/index.js
@@ -7,18 +7,30 @@ const upload = require("./multer");
 
 passport.use(new localStrategy(userModel.authenticate()));
 
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) return next();
+  res.redirect("/");
+}
+
 /* GET profile page. */
 router.get("/profile", async function (req, res, next) {
-  const user =  await userModel.findOne({username: req.session.passport.user})
-  res.render("profile", {user});
+  // const user = await userModel.findOne({ username: req.session.passport.user });
+  // res.render("profile", { user });
+  res.render("profile");
 });
 
-router.post("/fileupload", upload.single("image"), async function (req, res, next) {
-  const user =  await userModel.findOne({username: req.session.passport.user})
-  user.profileImage = req.file.filename;
-  await user.save()
-  res.redirect("/profile")
-});
+router.post(
+  "/fileupload",
+  upload.single("image"),
+  async function (req, res, next) {
+    const user = await userModel.findOne({
+      username: req.session.passport.user,
+    });
+    user.profileImage = req.file.filename;
+    await user.save();
+    res.redirect("/profile");
+  }
+);
 
 /* GET login page. */
 router.get("/", function (req, res, next) {
@@ -31,36 +43,17 @@ router.get("/register", function (req, res, next) {
 });
 
 /* Post register page. */
-// router.post("/register", function (req, res, next) {
-//   const data = new userModel({
-//     username: req.body.username,
-//     email: req.body.email,
-//   });
-//   userModel.register(data, req.body.cPassword).then(function () {
-//     passport.authenticate("local")(req, res, function () {
-//       res.redirect("/profile");
-//     });
-//   });
-// });
-router.post("/register", isLoggedIn, async function (req, res, next) {
-  try {
-    const data = new userModel({
-      username: req.body.username,
-      email: req.body.email,
-    });
-
-    await userModel.register(data, req.body.cPassword);
-
+router.post("/register", function (req, res, next) {
+  const data = new userModel({
+    username: req.body.username,
+    email: req.body.email,
+  });
+  userModel.register(data, req.body.cPassword).then(function () {
     passport.authenticate("local")(req, res, function () {
       res.redirect("/profile");
     });
-  } catch (error) {
-    console.error(error);
-    // Handle the error (e.g., redirect to an error page)
-  }
+  });
 });
-
-
 
 /* Post login page. */
 router.post(
@@ -80,12 +73,5 @@ router.get("/logout", function (req, res, next) {
     res.redirect("/");
   });
 });
-
-function isLoggedIn(req, res, next) {
-  if (req.isAuthenticated()) {
-    return next();
-  }
-  res.redirect("/");
-}
 
 module.exports = router;
